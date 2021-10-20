06f2947fd8eb611fe06db3f983feae3445ca787d
Chitsanupong <soda200852@gmail.com>
2021-10-20 12:52:57 +0700

    create master page

---
+++ views/layout/masterpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../assets/css/style.css">
+    <title>AssetsMangements</title>
+</head>
+
+<body>
+    <div class="header">
+
+    </div>
+    <div class="nav">
+        <ul>
+            <li>hi</li>
+            <li>hi</li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+</body>
+
+</html>
