0959f527f463784a211cab2201a0eb86358e6cd4
chitsanupong <chitsanupong.boonserm@gmail.com>
2021-11-26 01:59:12 +0700

    add label

--- views/create-money-source/create-money-source.php
+++ views/create-money-source/create-money-source.php
@@ -8,6 +8,7 @@ if (empty($_SESSION['username'])) {
     <h1 style="margin-left:10%;">สร้างข้อมูลแหล่งเงิน</h1>
     <form action="../../assets/db/insertmoney-source.php" method="post">
         <div class="row" style="margin: 10px 0 10px 39rem; width:50%;">
+        <label>ชื่อแหล่งเงิน</label>
             <input type="text" class="form-control" name="sourcename" placeholder="กรอกแหล่งเงิน">
         </div>
         <div class="row" style="margin: 10px 0 10px 39rem; width:50%;">
